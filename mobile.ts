4a6eba00522600faab9cecba711fa6e0fdc186c0
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-03-29 07:40:02 +0530

    Day-3 Mobile CRUD operations

---
+++ mobile.ts
@@ -0,0 +1,16 @@
+export class Mobile {
+
+    
+    MobileId:number;
+    MobileName:string;
+    MobilePrice:number;
+
+    constructor(MobileId:number,MobileName:string,MobilePrice:number){
+        this.MobileId=MobileId;
+        this.MobileName=MobileName;
+        this.MobilePrice=MobilePrice;
+    }
+
+}
+
+
